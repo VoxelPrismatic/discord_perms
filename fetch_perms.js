5fb636db3957d7618cd7f231dea68d2a23387385
PRIZ ;] <prizmdev@outlook.com>
2024-03-07 10:53:27 -0600

    Update fetch_perms.js

--- fetch_perms.js
+++ fetch_perms.js
@@ -2,7 +2,7 @@ j = "perms = {"
 for(var row of document.querySelector("h6#permissions-bitwise-permission-flags + table > tbody").children) {
     perm = row.children[0].textContent;
     j += `\n`;
-    j += `    "${perm.endsWith(" *") ? perm.slice(0, -2) : perm}":`.padEnd(35);
+    j += `    "${perm.replace(/\ \**$/g, "")}":`.padEnd(48);
     j += `${row.children[1].textContent.split(' ')[0]}n,`;
 }
 j = j.slice(0, -1) + "\n}";
